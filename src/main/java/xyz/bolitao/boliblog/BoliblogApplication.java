20e6aad3413865d89e3fceca4f25ad305a742532
Boli Tao <tblsyx@outlook.com>
2020-12-26 13:29:27 +0800

    git init

---
+++ src/main/java/xyz/bolitao/boliblog/BoliblogApplication.java
@@ -0,0 +1,16 @@
+package xyz.bolitao.boliblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bolitao
+ */
+@SpringBootApplication
+public class BoliblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoliblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/bolitao/boliblog/BoliblogApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.bolitao.boliblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoliblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
