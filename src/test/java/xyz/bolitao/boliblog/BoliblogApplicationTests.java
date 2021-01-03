7c89c892ef8b81ce4af1fa421a3b378a6447611f
Boli Tao <tblsyx@outlook.com>
2021-01-03 10:16:39 +0800

    remove p6spy

--- src/test/java/xyz/bolitao/boliblog/BoliblogApplicationTests.java
+++ src/test/java/xyz/bolitao/boliblog/BoliblogApplicationTests.java
@@ -1,13 +1,13 @@
-package xyz.bolitao.boliblog;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BoliblogApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package xyz.bolitao.boliblog;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BoliblogApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
